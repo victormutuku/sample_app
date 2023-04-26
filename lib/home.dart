62bec8316bb0bad8674015813628642e7e07b1cc
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-04-26 09:32:57 +0300

    So far update

--- lib/home.dart
+++ lib/home.dart
@@ -41,9 +41,21 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final deviceHeight = MediaQuery.of(context).size.height;
+    final containerHeight =
+        myValue == 0 ? deviceHeight * 0.1 : deviceHeight * 0.4;
     final items = Provider.of<Items>(context).items;
     return Scaffold(
       appBar: AppBar(
+        leading: myValue == 0
+            ? null
+            : IconButton(
+                onPressed: () {
+                  setState(() {
+                    myValue = 0;
+                  });
+                },
+                icon: const Icon(Icons.arrow_back),
+              ),
         title: const Text('Home'),
         centerTitle: true,
       ),
@@ -51,26 +63,76 @@ class _HomeScreenState extends State<HomeScreen> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              // SizedBox(
-              //   height: deviceHeight * 0.5,
-              //   child: PageView.builder(
-              //     itemCount: items.length,
-              //     onPageChanged: (value) {},
-              //     controller: controller,
-              //     itemBuilder: (context, index) => builder(index),
-              //   ),
-              // ),
-              Container(
-                height: deviceHeight * 0.4,
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 1000),
+                curve: Curves.easeInCubic,
+                height: containerHeight,
                 padding: const EdgeInsets.symmetric(
                   horizontal: 24.0,
                 ),
                 child: Card(
-                  color: Colors.blue.withOpacity(0.3),
-                  child: Center(
-                    child: Text(
-                      myValue.toString(),
-                      style: const TextStyle(fontSize: 40),
+                  color: const Color.fromARGB(255, 165, 214, 255),
+                  child: SizedBox(
+                    height: double.infinity,
+                    width: double.infinity,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AnimatedContainer(
+                            duration: const Duration(milliseconds: 1000),
+                            height: containerHeight < 100 ? 10 : 100,
+                          ),
+                          Text(
+                            myValue.toString(),
+                            style: const TextStyle(fontSize: 40),
+                          ),
+                          containerHeight < 100
+                              ? const SizedBox()
+                              : Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    OutlinedButton(
+                                      onPressed: () {},
+                                      style: OutlinedButton.styleFrom(
+                                        padding:
+                                            const EdgeInsets.only(right: 4),
+                                        side: BorderSide.none,
+                                      ),
+                                      child: Row(
+                                        children: const [
+                                          Icon(Icons.play_arrow_outlined),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text('Watch Now')
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                    OutlinedButton(
+                                      onPressed: () {},
+                                      style: OutlinedButton.styleFrom(
+                                        padding:
+                                            const EdgeInsets.only(right: 4),
+                                        side: BorderSide.none,
+                                      ),
+                                      child: Row(
+                                        children: const [
+                                          Icon(Icons.add),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text('WishList')
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                )
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -96,38 +158,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  builder(int index) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (context, child) {
-        double value = 1.0;
-        if (controller.position.haveDimensions) {
-          value = controller.page! - index;
-          value = (1 - (value.abs() * .4)).clamp(0.0, 1.0);
-        }
-        return SizedBox(
-          height: Curves.easeOut.transform(value) * 300,
-          width: Curves.easeOut.transform(value) * 300,
-          child: child,
-        );
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24.0,
-        ),
-        child: Card(
-          color: Colors.blue.withOpacity(0.3),
-          child: Center(
-            child: Text(
-              index.toString(),
-              style: const TextStyle(fontSize: 40),
-            ),
           ),
         ),
       ),

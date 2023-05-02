7d4729ccd3c42954470da140b9300cc277678b4b
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-05-02 09:02:17 +0300

    Completed the required task

--- lib/home.dart
+++ lib/home.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
     controller = PageController(
       initialPage: currentpage,
       keepPage: false,
-      viewportFraction: 1,
+      viewportFraction: 0.8,
     );
     super.initState();
   }
@@ -63,102 +63,153 @@ class _HomeScreenState extends State<HomeScreen> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 1000),
-                curve: Curves.easeInCubic,
-                height: containerHeight,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24.0,
-                ),
-                child: Card(
-                  color: const Color.fromARGB(255, 165, 214, 255),
-                  child: SizedBox(
-                    height: double.infinity,
-                    width: double.infinity,
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          AnimatedContainer(
-                            duration: const Duration(milliseconds: 1000),
-                            height: containerHeight < 100 ? 10 : 100,
-                          ),
-                          Text(
-                            myValue.toString(),
-                            style: const TextStyle(fontSize: 40),
-                          ),
-                          containerHeight < 100
-                              ? const SizedBox()
-                              : Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    OutlinedButton(
-                                      onPressed: () {},
-                                      style: OutlinedButton.styleFrom(
-                                        padding:
-                                            const EdgeInsets.only(right: 4),
-                                        side: BorderSide.none,
-                                      ),
-                                      child: Row(
-                                        children: const [
-                                          Icon(Icons.play_arrow_outlined),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text('Watch Now')
-                                        ],
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      width: 20,
-                                    ),
-                                    OutlinedButton(
-                                      onPressed: () {},
-                                      style: OutlinedButton.styleFrom(
-                                        padding:
-                                            const EdgeInsets.only(right: 4),
-                                        side: BorderSide.none,
-                                      ),
-                                      child: Row(
-                                        children: const [
-                                          Icon(Icons.add),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text('WishList')
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.5,
-                child: ListView.builder(
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    return Consumer<Items>(
-                      builder: (context, item, _) => ListTile(
-                        onTap: () {
-                          setState(() {
-                            myValue = item.sqItems[index];
-                          });
-                        },
-                        title: Text(
-                          item.sqItems[index].toString(),
+              myValue == 0
+                  ? Hero(
+                      tag: myValue,
+                      child: SizedBox(
+                        height: deviceHeight * 0.3,
+                        child: PageView.builder(
+                          itemCount: items.length,
+                          onPageChanged: (value) {},
+                          controller: controller,
+                          itemBuilder: (context, index) => builder(index),
                         ),
                       ),
-                    );
-                  },
+                    )
+                  : Hero(
+                      tag: myValue,
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 1000),
+                        curve: Curves.easeInCubic,
+                        height: containerHeight,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24.0,
+                        ),
+                        child: Card(
+                          color: const Color.fromARGB(255, 165, 214, 255),
+                          child: SizedBox(
+                            height: double.infinity,
+                            width: double.infinity,
+                            child: SingleChildScrollView(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  AnimatedContainer(
+                                    duration:
+                                        const Duration(milliseconds: 1000),
+                                    height: containerHeight < 100 ? 10 : 100,
+                                  ),
+                                  Text(
+                                    myValue.toString(),
+                                    style: const TextStyle(fontSize: 40),
+                                  ),
+                                  containerHeight < 100
+                                      ? const SizedBox()
+                                      : Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            OutlinedButton(
+                                              onPressed: () {},
+                                              style: OutlinedButton.styleFrom(
+                                                padding: const EdgeInsets.only(
+                                                  right: 4,
+                                                ),
+                                                side: BorderSide.none,
+                                              ),
+                                              child: Row(
+                                                children: const [
+                                                  Icon(Icons
+                                                      .play_arrow_outlined),
+                                                  SizedBox(
+                                                    width: 5,
+                                                  ),
+                                                  Text('Watch Now')
+                                                ],
+                                              ),
+                                            ),
+                                            const SizedBox(
+                                              width: 20,
+                                            ),
+                                            OutlinedButton(
+                                              onPressed: () {},
+                                              style: OutlinedButton.styleFrom(
+                                                padding: const EdgeInsets.only(
+                                                  right: 4,
+                                                ),
+                                                side: BorderSide.none,
+                                              ),
+                                              child: Row(
+                                                children: const [
+                                                  Icon(Icons.add),
+                                                  SizedBox(
+                                                    width: 5,
+                                                  ),
+                                                  Text('WishList')
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+              Hero(
+                tag: myValue,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return Consumer<Items>(
+                        builder: (context, item, _) => ListTile(
+                          onTap: () {
+                            setState(() {
+                              myValue = item.sqItems[index];
+                            });
+                          },
+                          title: Text(
+                            item.sqItems[index].toString(),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  builder(int index) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        double value = 1.0;
+        if (controller.position.haveDimensions) {
+          value = controller.page! - index;
+          value = (1 - (value.abs() * .4)).clamp(0.0, 1.0);
+        }
+        return SizedBox(
+          height: Curves.easeOut.transform(value) * 400,
+          width: Curves.easeOut.transform(value) * 400,
+          child: child,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(34),
+          ),
+          color: Colors.amber,
         ),
       ),
     );
